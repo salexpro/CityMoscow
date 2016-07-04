e705db4d1e4082f6d62b2495e5098fbacda7b8ed
salexpro <dev@salex.pro>
2016-07-04 15:09:38 +0300

    Rewrite adaptive main

--- js/app.js
+++ js/app.js
@@ -16,37 +16,37 @@ $('.towers a').mouseover(function() {
     $('img', this).attr('src', $('img', this).attr('src').substr(0, $('img', this).attr('src').indexOf('-h.png')) + '.png');
     $(this).prev().hide();
 })
-var $grid = $('.grid').masonry({
-    itemSelector: '.tile',
-    columnWidth: 195,
-    gutter: 15,
-    fitWidth: true
-});
-var $grid_half = $('.grid_half').masonry({
-    itemSelector: '.grid_half>.tile',
-    columnWidth: 300,
-    gutter: 15,
-    fitWidth: true
-});
-if ($(window).width() <= 645) {
-    $grid.masonry({
-        columnWidth: 405
-    });
-}
-$(window).resize(function() {
-    if ($(window).width() <= 645) {
-        $grid_half.masonry({
-            columnWidth: 195
-        });
-    } else {
-        $grid_half.masonry({
-            columnWidth: 300
-        });
-    }
-})
-if($(window).width()<=435){
-    $('[name="viewport"]').attr('content','width=436')
-}
+// var $grid = $('.grid').masonry({
+//     itemSelector: '.tile',
+//     columnWidth: 195,
+//     gutter: 15,
+//     fitWidth: true
+// });
+// var $grid_half = $('.grid_half').masonry({
+//     itemSelector: '.grid_half>.tile',
+//     columnWidth: 300,
+//     gutter: 15,
+//     fitWidth: true
+// });
+// if ($(window).width() <= 645) {
+//     $grid.masonry({
+//         columnWidth: 405
+//     });
+// }
+// $(window).resize(function() {
+//     if ($(window).width() <= 645) {
+//         $grid_half.masonry({
+//             columnWidth: 195
+//         });
+//     } else {
+//         $grid_half.masonry({
+//             columnWidth: 300
+//         });
+//     }
+// })
+// if($(window).width()<=435){
+//     $('[name="viewport"]').attr('content','width=436')
+// }
 // if ($(window).width() <= 420) {
 //     $('.grid').masonry({
 //         // options
