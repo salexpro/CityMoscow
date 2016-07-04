519c850fdd7b78fe306a8af7fc34524c5be73c44
salexpro <dev@salex.pro>
2016-07-04 12:50:22 +0300

    Skyline adaptive + icons fix

--- js/app.js
+++ js/app.js
@@ -16,45 +16,62 @@ $('.towers a').mouseover(function() {
     $('img', this).attr('src', $('img', this).attr('src').substr(0, $('img', this).attr('src').indexOf('-h.png')) + '.png');
     $(this).prev().hide();
 })
-$grid = $('.grid').masonry({
-    // options
+var $grid = $('.grid').masonry({
     itemSelector: '.tile',
     columnWidth: 195,
     gutter: 15,
     fitWidth: true
 });
-$('.grid_half').masonry({
-    // options
+var $grid_half = $('.grid_half').masonry({
     itemSelector: '.grid_half>.tile',
     columnWidth: 300,
     gutter: 15,
     fitWidth: true
 });
-if ($(window).width() <= 420) {
-    $('.grid').masonry({
-        // options
-        itemSelector: '.tile',
-        columnWidth: 150,
-        gutter: 7.5,
-        fitWidth: true
+if ($(window).width() <= 645) {
+    $grid.masonry({
+        columnWidth: 405
     });
 }
 $(window).resize(function() {
-    if ($(window).width() <= 420) {
-        $('.grid').masonry({
-            // options
-            itemSelector: '.tile',
-            columnWidth: 150,
-            gutter: 7.5,
-            fitWidth: true
+    if ($(window).width() <= 645) {
+        $grid_half.masonry({
+            columnWidth: 195
         });
     } else {
-        $('.grid').masonry({
-            // options
-            itemSelector: '.tile',
-            columnWidth: 195,
-            gutter: 15,
-            fitWidth: true
+        $grid_half.masonry({
+            columnWidth: 300
         });
     }
 })
+if($(window).width()<=435){
+    $('[name="viewport"]').attr('content','width=436')
+}
+// if ($(window).width() <= 420) {
+//     $('.grid').masonry({
+//         // options
+//         itemSelector: '.tile',
+//         columnWidth: 150,
+//         gutter: 7.5,
+//         fitWidth: true
+//     });
+// }
+// $(window).resize(function() {
+//     if ($(window).width() <= 420) {
+//         $('.grid').masonry({
+//             // options
+//             itemSelector: '.tile',
+//             columnWidth: 150,
+//             gutter: 7.5,
+//             fitWidth: true
+//         });
+//     } else {
+//         $('.grid').masonry({
+//             // options
+//             itemSelector: '.tile',
+//             columnWidth: 195,
+//             gutter: 15,
+//             fitWidth: true
+//         });
+//     }
+// })
